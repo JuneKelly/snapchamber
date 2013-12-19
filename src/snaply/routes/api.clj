dc40b2b719415f221db6a4f2254b78245a9508a0
Shane Kilkelly <shane@kilkelly.me>
2013-12-19 18:58:34 +0000

    begin filling out snap api

--- src/snaply/routes/api.clj
+++ src/snaply/routes/api.clj
@@ -1,22 +1,37 @@
 (ns snaply.routes.api
   (:use compojure.core)
-  (:require [liberator.core :refer [defresource]]))
+  (:require [liberator.core
+             :refer [defresource
+                     request-method-in]]))
 
 
-(def things
-  ["Clojure",
-   "Liberator",
-   "AngularJS",
-   "CoffeeScript",
-   "Sass",
-   "Grunt",
-   "Bower"])
+(defn create-snap
+  [context]
+  (println (get-in context [:request :params])))
 
 
-(defresource nice-things
+(defn get-snap-id
+  [context]
+  (comment "todo"))
+
+
+(defresource snap
+  :service-available? true
   :available-media-types ["application/json"]
-  :handle-ok {:things things})
+  :method-allowed? (request-method-in :get :post)
+
+  :malformed?
+  (fn [context]
+    (let [params (get-in context [:request :params])]
+      (empty? (params :imageData))))
+
+  :handle-malformed
+  (fn [_]
+    (str "Error: imageData required"))
+
+  :post! create-snap
+  :handle-created (fn [_] {:snapId "asdf"}))
 
 
 (defroutes api-routes
-  (GET "/api/nicethings" [] nice-things))
+  (ANY "/api/snap" [] snap))
